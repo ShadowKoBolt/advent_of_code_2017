4bdf2c01a006abef9758172d26b70131fa445a9a
Nick Bolt <nick.bolt@redemptionmedia.co.uk>
2017-12-01 14:11:22 +0000

    Day 1 part 2 challenge
    Deal with looped array by rotating

---
+++ day1_part2.rb
@@ -0,0 +1,20 @@
+require 'pry'
+require 'pry-nav'
+Examples = %w[1212 1221 123425 123123 12131415]
+Results = [6, 0, 4, 12, 4]
+
+module Captcha
+  def call(input)
+    chars = input.to_s.chars
+    chars.length.times.map do |i|
+      rotated_chars = chars.rotate(i)
+      rotated_chars[0] == rotated_chars[chars.length/2] ? rotated_chars[0].to_i : 0
+    end.sum
+  end
+
+  module_function :call
+end
+
+Examples.each_with_index do |example, i|
+  raise(StandardError, "#{example} should eql #{i}") unless Captcha.call(example) == Results[i]
+end
